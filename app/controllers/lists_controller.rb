6d6befc11c66d13e0b21163c9bf37dccaaef00b2
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-06 06:00:07 +0000

    [finish]3-9

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,54 @@
+class ListsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      ridirect_to list_path(@list.id)
+    else
+      render :index #<= new から indexに変更
+    end
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
+
+  def index
+    @lists = List.all
+
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  private
+
+   def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
